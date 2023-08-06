451ab247aba6e3234b81e171cd27062f6cba1d47
MikhailWahib <mikhailwahib1@gmail.com>
2023-08-06 08:41:14 +0200

    Made the Login Page responsive

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<main className='min-h-screen text-[#fefefe] overflow-hidden'>
-			<div className='absolute top-12 left-16'>
+			<div className='absolute top-5 left-6 md:top-12 md:left-16 z-50'>
 				<Link href='/'>
 					<Image src={logo} width={50} height={50} alt='Movibes Logo' />
 				</Link>
@@ -18,14 +18,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 			<div className='flex'>
 				{children}
 				{/* POSTER SECTION */}
-				<div className='relative login__poster h-screen flex-[55%]'>
-					<div className='absolute flex w-full bottom-0 h-[210px] pt-6 pl-7 bg-[#19191908] backdrop-filter backdrop-blur-[2px] transition-all duration-500 translate-y-[0%]'>
-						<div className='flex-[80%]'>
+				<div className='hidden md:block relative login__poster h-screen flex-[55%]'>
+					<div className='absolute flex w-full bottom-0 h-[210px] pt-6 px-7 bg-[#19191908] backdrop-filter backdrop-blur-[2px] transition-all duration-500 translate-y-[0%]'>
+						<div className='flex-1'>
 							<h2 className='text-4xl font-semibold mb-1'>Top Gun: Maverick</h2>
 							<p className='text-xl'>2022 | PG-13 | 2h 10m</p>
 							<p className='text-xl mt-4'>Genres : Action , Drama</p>
 						</div>
-						<div className='flex-[20%]'>
+						<div className='flex-shrink-1'>
 							<div className='flex items-center gap-2'>
 								<Image
 									src='/imdbLogo.png'
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,10 @@
 const Login = () => {
 	return (
-		<div className='flex-[45%] flex items-center justify-center h-screen text-center px-5'>
+		<div className='absolute h-screen w-full z-20 bg-[#191919] flex-[45%] flex items-center justify-center text-center px-5 md:relative md:inset-0'>
 			<div>
 				<div className='mb-20'>
-					<h1 className='text-3xl font-medium'>Welcome back, Olivia</h1>
-					<p className='text-xl text-[#BABABAB2] opacity-70'>
+					<h1 className='text-3xl font-medium'>Welcome back</h1>
+					<p className='text-base md:text-xl text-[#BABABAB2] opacity-70'>
 						Welcome back! Please enter your details.
 					</p>
 				</div>
