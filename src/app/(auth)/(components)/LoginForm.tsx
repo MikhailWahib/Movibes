84c11dbf9d33573a0a225a960e69c3fb90141954
MikhailWahib <mikhailwahib1@gmail.com>
2023-08-08 16:00:36 +0200

    Fixed login form

--- src/app/(auth)/(components)/LoginForm.tsx
+++ src/app/(auth)/(components)/LoginForm.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { Field, Form, Formik, FormikProps } from 'formik'
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik'
 import * as Yup from 'yup'
+import { useState } from 'react'
 
 const validationSchema = Yup.object().shape({
 	email: Yup.string()
@@ -19,11 +20,15 @@ interface FormValues {
 }
 
 const LoginForm = () => {
+	const [error, setError] = useState('')
 	const router = useRouter()
 
 	const initialValues: FormValues = { email: '', password: '' }
 
-	const handleSubmit = async (values: FormValues) => {
+	const handleSubmit = async (
+		values: FormValues,
+		actions: FormikHelpers<FormValues>
+	) => {
 		try {
 			const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/auth`, {
 				method: 'POST',
@@ -38,62 +43,65 @@ const LoginForm = () => {
 			if (res.ok) {
 				localStorage.setItem('user', JSON.stringify(userData))
 				router.push('/')
+			} else {
+				setError(userData.message)
 			}
 		} catch (error) {
-			console.log(error)
+			console.error(error)
 		}
 	}
 
 	return (
-		<>
-			<Formik
-				initialValues={initialValues}
-				validationSchema={validationSchema}
-				onSubmit={handleSubmit}
-			>
-				{(formikProps: FormikProps<FormValues>) => (
-					<Form className='flex flex-col gap-7' noValidate>
-						<div className='relative'>
-							<Field
-								autoFocus
-								type='email'
-								name='email'
-								placeholder='Email'
-								className='w-full bg-transparent border-[#4D4B4B] border-b-[1px] p-2 outline-none focus:border-[#3DD2CC] transition-all'
-							/>
+		<Formik
+			initialValues={initialValues}
+			validationSchema={validationSchema}
+			onSubmit={handleSubmit}
+		>
+			<Form className='flex flex-col gap-7' noValidate>
+				<div className='relative'>
+					<Field
+						autoFocus
+						type='email'
+						name='email'
+						placeholder='Email'
+						className='w-full bg-transparent border-[#4D4B4B] border-b-[1px] p-2 outline-none focus:border-[#3DD2CC] transition-all'
+					/>
 
-							{formikProps.touched.email && formikProps.errors.email && (
-								<div className='absolute text-red-500'>
-									{formikProps.errors.email}
-								</div>
-							)}
-						</div>
+					<ErrorMessage
+						name='email'
+						component='div'
+						className='absolute text-red-500 text-sm'
+					/>
+				</div>
 
-						<div className='relative'>
-							<Field
-								type='password'
-								name='password'
-								placeholder='Password'
-								className='w-full bg-transparent border-[#4D4B4B] border-b-[1px] p-2 outline-none focus:border-[#3DD2CC] transition-all'
-							/>
+				<div className='relative'>
+					<Field
+						type='password'
+						name='password'
+						placeholder='Password'
+						className='w-full bg-transparent border-[#4D4B4B] border-b-[1px] p-2 outline-none focus:border-[#3DD2CC] transition-all'
+					/>
 
-							{formikProps.touched.password && formikProps.errors.password && (
-								<div className='absolute text-red-500'>
-									{formikProps.errors.password}
-								</div>
-							)}
-						</div>
+					<ErrorMessage
+						name='password'
+						component='div'
+						className='absolute text-red-500 text-sm'
+					/>
+				</div>
+				<div className='relative'>
+					{error ? (
+						<p className='absolute text-center w-full text-red-500'>{error}</p>
+					) : null}
+				</div>
 
-						<button
-							type='submit'
-							className='h-14 mt-10 text-[#191919] text-xl font-semibold bg-[#FEFEFE] rounded-2xl transition-all hover:opacity-60'
-						>
-							Login
-						</button>
-					</Form>
-				)}
-			</Formik>
-		</>
+				<button
+					type='submit'
+					className='h-14 mt-10 text-[#191919] text-xl font-semibold bg-[#FEFEFE] rounded-2xl transition-all hover:opacity-60'
+				>
+					Login
+				</button>
+			</Form>
+		</Formik>
 	)
 }
 
