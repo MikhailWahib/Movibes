41e47af344d97db3b88eacc8eecd2adf89d8b81f
MikhailWahib <mikhailwahib1@gmail.com>
2023-08-10 06:42:08 +0200

    Modified auth/Poster Data

--- src/app/(auth)/(components)/Poster.tsx
+++ src/app/(auth)/(components)/Poster.tsx
@@ -18,7 +18,7 @@ const Poster = async ({ movie }: Props) => {
 		>
 			<div className='absolute flex w-full bottom-0 h-[210px] pt-6 px-7 bg-[#19191908] backdrop-filter backdrop-blur-[2px] transition-all duration-500 translate-y-[0]'>
 				<div className='flex-1'>
-					<h2 className='text-4xl font-semibold mb-1 text-yellow-500 '>
+					<h2 className='text-4xl font-semibold mb-1 text-yellow-500'>
 						{movie?.title}
 					</h2>
 					<p className='text-xl'>{`${movie?.year} | ${movie?.rate} | ${movie?.duration}`}</p>
@@ -36,7 +36,7 @@ const Poster = async ({ movie }: Props) => {
 							alt='Star vector'
 							className='h-[20px] w-[20px]'
 						/>
-						<p className='font-semibold'>8.5 / 10</p>
+						<p className='font-semibold'>{movie?.rateOutOf10} / 10</p>
 					</div>
 				</div>
 			</div>
