bfcc6b1576c144ca12debb23698991c1ad810f79
MikhailWahib <mikhailwahib1@gmail.com>
2023-08-15 15:24:41 +0200

    Finish Topbar

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -1,15 +1,37 @@
+'use client'
+
+import { useState } from 'react'
+
+import Image from 'next/image'
 import Searchbar from './Searchbar'
+import avatarImg from '../../../public/avatarImg.jpg'
 import { BiSolidBellRing } from 'react-icons/bi'
 
 const Topbar = () => {
+	const [openNotifications, setOpenNotifications] = useState(false)
+
 	return (
 		<div className='w-full flex items-center justify-between'>
 			<Searchbar />
 			<div className='flex gap-2'>
-				<button className='h-[40px] w-[40px] flex items-center justify-center bg-[#212121] rounded-full '>
+				<button
+					className='relative h-[40px] w-[40px] flex items-center justify-center bg-[#212121] rounded-full'
+					onClick={() => setOpenNotifications(!openNotifications)}
+				>
 					<BiSolidBellRing />
+					<div
+						className={`absolute top-14 flex min-h-[128px] w-48 bg-[#212121] rounded-lg z-50 transition-all ${
+							openNotifications ? 'scale-100' : 'scale-0'
+						}`}
+					>
+						<p className='self-center w-full text-sm opacity-70'>
+							No notifications.
+						</p>
+					</div>
 				</button>
-				<div className='h-[40px] w-[40px] bg-[#212121] rounded-full'></div>
+				<div className='relative h-[40px] w-[40px] bg-[#212121] rounded-full overflow-clip'>
+					<Image src={avatarImg} width={40} height={40} alt='Avatar' />
+				</div>
 			</div>
 		</div>
 	)
