4f52aaf9604ca4939a822c7184a88b477b9b3eb6
MikhailWahib <mikhailwahib1@gmail.com>
2023-08-28 14:06:06 +0200

    Fix home layout padding on small screens

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -12,7 +12,7 @@ export default function HomeLayout({
 		<div className='h-screen w-full text-white'>
 			<SidebarProvider>
 				<Sidebar />
-				<div className='flex-1 px-10 md:pl-64 md:pr-11 overflow-x-hidden pt-9'>
+				<div className='flex-1 px-4 md:pl-64 md:pr-11 overflow-x-hidden pt-9'>
 					<Topbar />
 					<main className='py-5'>{children}</main>
 				</div>
