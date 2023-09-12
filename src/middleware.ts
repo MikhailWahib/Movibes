d454c4cb47efa86740d00cfc7b05254c7f9df357
MikhailWahib <mikhailwahib1@gmail.com>
2023-09-12 17:56:50 +0200

    Fix redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,13 +16,13 @@ export function middleware(request: NextRequest) {
 		return NextResponse.redirect(new URL('/login', request.url))
 	}
 
-	if (jwt && request.nextUrl.pathname === '/login') {
-		return NextResponse.redirect(new URL('/home', request.url))
-	}
+	// if (jwt && request.nextUrl.pathname === '/login') {
+	// 	return NextResponse.redirect(new URL('/home', request.url))
+	// }
 
-	if (jwt && request.nextUrl.pathname === '/signup') {
-		return NextResponse.redirect(new URL('/home', request.url))
-	}
+	// if (jwt && request.nextUrl.pathname === '/signup') {
+	// 	return NextResponse.redirect(new URL('/home', request.url))
+	// }
 }
 
 export const config = {
