414fb47c3a516381f79b7f3b31659d95e6c4b2cf
MikhailWahib <mikhailwahib1@gmail.com>
2023-11-04 17:42:27 +0200

    Add login as guest

--- src/app/(auth)/components/LoginForm.tsx
+++ src/app/(auth)/components/LoginForm.tsx
@@ -1,18 +1,18 @@
-'use client'
+"use client"
 
-import { useRouter } from 'next/navigation'
-import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik'
-import * as Yup from 'yup'
-import { useState } from 'react'
-import LogoSpinner from '@/app/components/LogoSpinner'
+import { useRouter } from "next/navigation"
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik"
+import * as Yup from "yup"
+import { useState } from "react"
+import LogoSpinner from "@/app/components/LogoSpinner"
 
 const validationSchema = Yup.object().shape({
 	email: Yup.string()
-		.email('Please enter a valid email address')
-		.required('Please enter your email'),
+		.email("Please enter a valid email address")
+		.required("Please enter your email"),
 	password: Yup.string()
-		.min(6, 'Password must be at least 6 characters')
-		.required('Please enter your password'),
+		.min(6, "Password must be at least 6 characters")
+		.required("Please enter your password"),
 })
 
 interface FormValues {
@@ -22,30 +22,58 @@ interface FormValues {
 
 const LoginForm = () => {
 	const [isLoading, setIsLoading] = useState(false)
-	const [error, setError] = useState('')
+	const [error, setError] = useState("")
 	const router = useRouter()
 
-	const initialValues: FormValues = { email: '', password: '' }
+	const initialValues: FormValues = { email: "", password: "" }
 
 	const handleSubmit = async (
 		values: FormValues,
 		actions: FormikHelpers<FormValues>
 	) => {
 		try {
-			setError('')
+			setError("")
 			setIsLoading(true)
-			const res = await fetch('api/auth/login', {
-				method: 'POST',
+			const res = await fetch("api/auth/login", {
+				method: "POST",
 				headers: {
-					'Content-Type': 'application/json',
+					"Content-Type": "application/json",
 				},
-				mode: 'same-origin',
-				credentials: 'include',
+				mode: "same-origin",
+				credentials: "include",
 				body: JSON.stringify(values),
 			})
 			const userData = await res.json()
 			if (res.ok) {
-				router.push('/home')
+				router.push("/home")
+			} else {
+				setError(userData.message)
+			}
+			setIsLoading(false)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	const handleGuestLogin = async () => {
+		try {
+			setError("")
+			setIsLoading(true)
+			const res = await fetch("api/auth/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				mode: "same-origin",
+				credentials: "include",
+				body: JSON.stringify({
+					email: "john@doe.com",
+					password: "123456",
+				}),
+			})
+			const userData = await res.json()
+			if (res.ok) {
+				router.push("/home")
 			} else {
 				setError(userData.message)
 			}
@@ -77,7 +105,6 @@ const LoginForm = () => {
 						className='absolute text-red-500 text-sm'
 					/>
 				</div>
-
 				<div className='relative'>
 					<Field
 						type='password'
@@ -102,7 +129,15 @@ const LoginForm = () => {
 					className='flex items-center justify-center h-14 mt-10 text-[#191919] text-xl font-semibold bg-[#FEFEFE] rounded-2xl transition-all hover:opacity-60'
 					disabled={isLoading}
 				>
-					{isLoading ? <LogoSpinner height={40} width={40} /> : 'Login'}
+					{isLoading ? <LogoSpinner height={40} width={40} /> : "Login"}
+				</button>
+				<button
+					onClick={handleGuestLogin}
+					type='button'
+					className='flex items-center justify-center h-14 text-lg bg-transparent border text-white rounded-2xl transition-all hover:opacity-60'
+					disabled={isLoading}
+				>
+					Login as Guest
 				</button>
 			</Form>
 		</Formik>
