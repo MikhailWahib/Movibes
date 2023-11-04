7a8c7e10cda1f698918eb42d2dc1a9f6e1bd658b
MikhailWahib <mikhailwahib1@gmail.com>
2023-11-04 10:48:13 +0200

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
+import "./globals.css"
+import type { Metadata } from "next"
+import { Poppins } from "next/font/google"
 
-import Sidebar from './components/Sidebar'
-import Topbar from './components/Topbar'
+import Sidebar from "./components/Sidebar"
+import Topbar from "./components/Topbar"
+
+import { Analytics } from "@vercel/analytics/react"
 
 const poppins = Poppins({
-	subsets: ['latin'],
-	weight: ['400', '500', '600', '700', '800', '900'],
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700", "800", "900"],
 })
 
 export const metadata: Metadata = {
-	title: 'Movibes',
-	description: 'The best place for movies lovers.',
+	title: "Movibes",
+	description: "The best place for movies lovers.",
 }
 
 export default function RootLayout({
@@ -22,7 +24,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={poppins.className}>{children}</body>
+			<body className={poppins.className}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	)
 }
