d976e247f75436032295a9cf921529a4ba3fdc6e
Mikhail <mikhailwahib1@gmail.com>
2025-09-04 07:56:33 +0300

    Fix

--- src/app/components/Billboard.tsx
+++ src/app/components/Billboard.tsx
@@ -1,49 +1,49 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-import { AiFillInfoCircle } from 'react-icons/ai'
-import { ShowDiscover } from '@/types'
-import { getData } from '@/api'
+import { AiFillInfoCircle } from "react-icons/ai";
+import { ShowDiscover } from "@/types";
+import { getData } from "@/api";
 
 const getShow = async (): Promise<ShowDiscover | undefined> => {
-	const show = getData('/trending/movie/day?language=en-US', {
-		next: { revalidate: 86400 },
-	}).then((res) => {
-		return res.results[0]
-	})
-	return show
-}
+  const show = getData("/trending/movie/day?language=en-US", {
+    next: { revalidate: 86400 },
+  }).then((res) => {
+    return res.results[0];
+  });
+  return show;
+};
 
 const Billboard = async () => {
-	const show = await getShow()
+  const show = await getShow();
 
-	return (
-		<div className='relative group h-[40vw] md:h-[25vw] flex w-full rounded-2xl overflow-hidden z-0'>
-			<div className='absolute inset-0'>
-				<Image
-					src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/original${show?.backdrop_path}`}
-					fill
-					alt='Poster'
-				></Image>
-			</div>
-			<div className='flex justify-between items-center mr-[4%] mb-[2%] self-end w-full text-sm font-normal text-[#e8e8e8]'>
-				<div className='relative'>
-					<div className='flex items-center max-h-[50px] w-[30vw] pl-3 bg-[#e8e8e819] backdrop-filter backdrop-blur-[10px] rounded-tr-lg rounded-br-lg text-[#3DD2CC] text-xs md:text-lg font-bold text-ellipsis overflow-hidden'>
-						<h2>{show?.title || show?.name}</h2>
-					</div>
-				</div>
+  return (
+    <div className="relative group h-[40vw] md:h-[25vw] flex w-full rounded-2xl overflow-hidden z-0">
+      <div className="absolute inset-0">
+        <Image
+          src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/original${show?.backdrop_path}`}
+          fill
+          alt="Poster"
+        ></Image>
+      </div>
+      <div className="flex justify-between items-center mr-[4%] mb-[2%] self-end w-full text-sm font-normal text-[#e8e8e8]">
+        <div className="relative">
+          <div className="flex items-center max-h-[50px] w-[30vw] pl-3 bg-[#e8e8e819] backdrop-filter backdrop-blur-[10px] rounded-tr-lg rounded-br-lg text-[#3DD2CC] text-xs md:text-lg font-bold text-ellipsis overflow-hidden">
+            <h2>{show?.title || show?.name}</h2>
+          </div>
+        </div>
 
-				<div className='flex gap-5'>
-					<Link href={`/show/${show?.id}?show_type=movie`}>
-						<div className='flex justify-center items-center gap-2 h-8 w-28 bg-[#e8e8e819] backdrop-filter backdrop-blur-[2.5px] rounded-lg transition-all hover:opacity-60'>
-							<AiFillInfoCircle />
-							More info
-						</div>
-					</Link>
-				</div>
-			</div>
-		</div>
-	)
-}
+        <div className="flex gap-5">
+          <Link href={`/${show?.title ? "movie" : "tv"}/${show?.id}`}>
+            <div className="flex justify-center items-center gap-2 h-8 w-28 bg-[#e8e8e819] backdrop-filter backdrop-blur-[2.5px] rounded-lg transition-all hover:opacity-60">
+              <AiFillInfoCircle />
+              More info
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Billboard
+export default Billboard;
--- src/app/components/Search/SearchResults.tsx
+++ src/app/components/Search/SearchResults.tsx
@@ -1,100 +1,98 @@
-'use client'
-import { useEffect, useState } from 'react'
+"use client";
+import { useEffect, useState } from "react";
 
-import Image from 'next/image'
+import Image from "next/image";
 
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
-import { SeachResponse } from '@/types'
+import { SeachResponse } from "@/types";
 
-import starIcon from '@/../public/star.png'
-import LogoSpinner from '../LogoSpinner'
+import starIcon from "@/../public/star.png";
+import LogoSpinner from "../LogoSpinner";
 
 interface Props {
-	query: string
-	setQuery: (val: string) => void
+  query: string;
+  setQuery: (val: string) => void;
 }
 
 const SearchResults = ({ query, setQuery }: Props) => {
-	const [data, setData] = useState<SeachResponse | undefined>()
-	const [loading, setLoading] = useState(false)
+  const [data, setData] = useState<SeachResponse | undefined>();
+  const [loading, setLoading] = useState(false);
 
-	const router = useRouter()
+  const router = useRouter();
 
-	const getSearchResults = async (query: string): Promise<void> => {
-		try {
-			setLoading(true)
-			const res = await fetch(`/api/search/${query}`)
-			const data = await res.json()
-			setData(data.data)
-			setLoading(false)
-		} catch (error) {
-			console.log(error)
-		}
-	}
+  const getSearchResults = async (query: string): Promise<void> => {
+    try {
+      setLoading(true);
+      const res = await fetch(`/api/search/${query}`);
+      const data = await res.json();
+      setData(data.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	useEffect(() => {
-		if (query) {
-			getSearchResults(query)
-		}
-	}, [query])
+  useEffect(() => {
+    if (query) {
+      getSearchResults(query);
+    }
+  }, [query]);
 
-	if (!query) return null
+  if (!query) return null;
 
-	return (
-		<div className='absolute inset-0 left-[5%] md:left-64 top-[5.5rem] w-[90%] md:w-[60%] max-h-[85vh] bg-[#212121] overflow-y-scroll rounded-3xl z-50'>
-			{loading ? (
-				<div className='relative h-full w-full flex justify-center items-center'>
-					<LogoSpinner />
-				</div>
-			) : (
-				<ul className='w-full h-full flex flex-col'>
-					{data?.results.map((show) => (
-						<li
-							key={show.id}
-							className='p-3 transition-all hover:bg-[#3dd2cc66] cursor-pointer'
-							onClick={() => {
-								router.push(
-									`/show/${show.id}?show_type=${show.title ? 'movie' : 'tv'}`
-								)
-								setQuery('')
-							}}
-						>
-							<div className='flex items-center gap-5 md:gap-10'>
-								<Image
-									src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/w500${show.poster_path}`}
-									width={75}
-									height={75}
-									className='rounded-2xl'
-									alt={`Poster for ${show.name || show.title}`}
-								/>
-								<div>
-									<h3 className='text-[#3DD2CC] text-md md:text-xl'>
-										{show.name || show.title}
-									</h3>
-									<p className='opacity-70 text-sm'>
-										{show.release_date || show.first_air_date}
-									</p>
-								</div>
-								<div className='ml-auto'>
-									<span className='text-xs opacity-70'>
-										{Math.round(show.vote_average * 10) / 10}/10
-									</span>
-									<Image
-										src={starIcon}
-										width={18}
-										height={18}
-										alt='Star'
-										className='inline-block relative -top-[1.5px] mx-[5px]'
-									/>
-								</div>
-							</div>
-						</li>
-					))}
-				</ul>
-			)}
-		</div>
-	)
-}
+  return (
+    <div className="absolute inset-0 left-[5%] md:left-64 top-[5.5rem] w-[90%] md:w-[60%] max-h-[85vh] bg-[#212121] overflow-y-scroll rounded-3xl z-50">
+      {loading ? (
+        <div className="relative h-full w-full flex justify-center items-center">
+          <LogoSpinner />
+        </div>
+      ) : (
+        <ul className="w-full h-full flex flex-col">
+          {data?.results.map((show) => (
+            <li
+              key={show.id}
+              className="p-3 transition-all hover:bg-[#3dd2cc66] cursor-pointer"
+              onClick={() => {
+                router.push(`/${show?.title ? "movie" : "tv"}/${show.id}`);
+                setQuery("");
+              }}
+            >
+              <div className="flex items-center gap-5 md:gap-10">
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/w500${show.poster_path}`}
+                  width={75}
+                  height={75}
+                  className="rounded-2xl"
+                  alt={`Poster for ${show.name || show.title}`}
+                />
+                <div>
+                  <h3 className="text-[#3DD2CC] text-md md:text-xl">
+                    {show.name || show.title}
+                  </h3>
+                  <p className="opacity-70 text-sm">
+                    {show.release_date || show.first_air_date}
+                  </p>
+                </div>
+                <div className="ml-auto">
+                  <span className="text-xs opacity-70">
+                    {Math.round(show.vote_average * 10) / 10}/10
+                  </span>
+                  <Image
+                    src={starIcon}
+                    width={18}
+                    height={18}
+                    alt="Star"
+                    className="inline-block relative -top-[1.5px] mx-[5px]"
+                  />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default SearchResults
+export default SearchResults;
